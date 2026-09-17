a7c423fea3157de3728cf4afc8c74f1f1778cf39
Vivek Goswami <vivekgoswami@Viveks-MacBook-Pro.local>
2026-09-18 01:44:32 +0530

    playlist changed

--- lib/playlists.ts
+++ lib/playlists.ts
@@ -34,16 +34,60 @@ export const goldenAge: Track[] = [
 
 export const epicAdventures: Track[] = [
   {
-    id: "moonlight-sonata",
-    title: "Moonlight Sonata",
-    artist: "Ludwig van Beethoven",
-    film: "Classical",
-    year: 1801,
+    id: "until-i-found-you",
+    title: "Until I Found You",
+    artist: "Stephen Sanchez",
+    film: "Music",
+    year: 2021,
+    duration: 180,
+    videoId: "GxldQ9eX2wo",
+  },
+  {
+    id: "die-with-a-smile",
+    title: "Die With A Smile",
+    artist: "Lady Gaga, Bruno Mars",
+    film: "Music",
+    year: 2024,
+    duration: 252,
+    videoId: "kPa7bsKwL-c",
+  },
+  {
+    id: "apocalypse",
+    title: "Apocalypse",
+    artist: "Cigarettes After Sex",
+    film: "Music",
+    year: 2017,
+    duration: 258,
+    videoId: "sElE_BfQ67s",
+  },
+  {
+    id: "cinnamon-girl",
+    title: "Cinnamon Girl",
+    artist: "Lana Del Rey",
+    film: "Music",
+    year: 2019,
     duration: 300,
-    videoId: "4Tr0otuiaMU",
+    videoId: "y1WBjdx-Q1E",
+  },
+  {
+    id: "wicked-game",
+    title: "Wicked Game",
+    artist: "Lusaint",
+    film: "Music",
+    year: 2021,
+    duration: 250,
+    videoId: "DDUptOux5v4",
+  },
+  {
+    id: "co2",
+    title: "Co2",
+    artist: "Prateek Kuhad",
+    film: "Music",
+    year: 2013,
+    duration: 165,
+    videoId: "U2SVCCENLjE",
   },
 ];
-
 export const indianMusic: Track[] = [
   {
     id: "musafir-cafe-darmiyaan",
@@ -54,6 +98,7 @@ export const indianMusic: Track[] = [
     duration: 180,
     videoId: "2pVM8Fs9qRI",
   },
+
   {
     id: "jab-tu-sajan",
     title: "Jab Tu Sajan",
@@ -63,6 +108,7 @@ export const indianMusic: Track[] = [
     duration: 240,
     videoId: "68gBaJ3RWxQ",
   },
+
   {
     id: "chori-chori",
     title: "Chori Chori",
@@ -72,6 +118,7 @@ export const indianMusic: Track[] = [
     duration: 210,
     videoId: "HnGpaCOEE5I",
   },
+
   {
     id: "ek-dil-ek-jaan",
     title: "Ek Dil Ek Jaan",
@@ -81,6 +128,7 @@ export const indianMusic: Track[] = [
     duration: 230,
     videoId: "c64I9HNpiOY",
   },
+
   {
     id: "parvati-full-song",
     title: "Parvati (Full Song)",
@@ -90,6 +138,7 @@ export const indianMusic: Track[] = [
     duration: 200,
     videoId: "rjfxLq3OQ0w",
   },
+
   {
     id: "shiv-kailash-live",
     title: "Shiv Kailash (Live in Mumbai)",
@@ -99,6 +148,7 @@ export const indianMusic: Track[] = [
     duration: 240,
     videoId: "Onb6_bRJ0Bw",
   },
+
   {
     id: "dooron-dooron-live",
     title: "Dooron Dooron (Live)",
@@ -108,6 +158,78 @@ export const indianMusic: Track[] = [
     duration: 220,
     videoId: "9T-Zbxg9X_4",
   },
+
+  // New tracks
+
+  {
+    id: "tere-paas-main",
+    title: "Tere Paas Main",
+    artist: "Diljit Dosanjh, A.R. Rahman",
+    film: "Tere Paas Main",
+    year: 2025,
+    duration: 210,
+    videoId: "k3AqQzW3cGI",
+  },
+
+  {
+    id: "hua-main-stripped",
+    title: "Hua Main (Stripped)",
+    artist: "Raghav Chaitanya",
+    film: "Animal",
+    year: 2023,
+    duration: 210,
+    videoId: "ZiiO2WHcPZc",
+  },
+
+  {
+    id: "sajni",
+    title: "Sajni",
+    artist: "Arijit Singh, Ram Sampath",
+    film: "Laapataa Ladies",
+    year: 2024,
+    duration: 170,
+    videoId: "XJAMVPQWvGY",
+  },
+
+  {
+    id: "yun-hi",
+    title: "Yun Hi",
+    artist: "Krsna Solo",
+    film: "Tanu Weds Manu",
+    year: 2011,
+    duration: 270,
+    videoId: "iwdPbQwZujY",
+  },
+
+  {
+    id: "monta-re",
+    title: "Monta Re",
+    artist: "Amit Trivedi",
+    film: "Lootera",
+    year: 2013,
+    duration: 210,
+    videoId: "99NUJ1cLbBI",
+  },
+
+  {
+    id: "aur-mohabbat-kitni-karoon",
+    title: "Aur Mohabbat Kitni Karoon",
+    artist: "Arijit Singh",
+    film: "Metro... In Dino",
+    year: 2025,
+    duration: 230,
+    videoId: "Uuce4UaHGQg",
+  },
+
+  {
+    id: "mann-mann-rangeyo",
+    title: "Mann Mann Rangeyo",
+    artist: "Shaan, Baribie Rajput",
+    film: "Hisaab Barabar",
+    year: 2024,
+    duration: 220,
+    videoId: "9JoMgqLIJsQ",
+  },
 ];
 
 export const playlists = {
@@ -115,16 +237,19 @@ export const playlists = {
     name: "Film Noir Classics",
     tracks: classicScores,
   },
+
   "golden-age": {
     name: "Golden Age Hollywood",
     tracks: goldenAge,
   },
+
   "epic-adventures": {
     name: "Epic Adventures",
     tracks: epicAdventures,
   },
+
   "indian-music": {
     name: "🎵 Indian Music",
     tracks: indianMusic,
   },
-};  
+};
